ced0aad71f7900668e23800965de5090c7753286
bharath-pingala <bharath@pingalasoftware.com>
2024-08-01 19:15:27 +0530

    required condition and scripts

--- script.js
+++ script.js
@@ -2,40 +2,46 @@ document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("contactForm");
 
   form.addEventListener("submit", function (e) {
-    e.preventDefault();
+    try {
+      e.preventDefault();
 
-    const firstName = document.getElementById("firstName").value;
-    const lastName = document.getElementById("lastName").value;
-    const email = document.getElementById("email").value;
-    const number = document.getElementById("phone").value;
-    const subject = document.getElementById("subject").value;
-    const message = document.getElementById("message").value;
+      const firstName = document.getElementById("firstName").value || "";
+      const lastName = document.getElementById("lastName").value || "";
+      const email = document.getElementById("email").value || "";
+      const number = document.getElementById("phone").value || "";
+      const telegram = document.getElementById("telegram").value || "";
+      const state = document.getElementById("state").value || "";
+      const country = document.getElementById("country").value || "";
+      const data = {
+        firstName,
+        lastName,
+        email,
+        number,
+        telegram,
+        state,
+        country,
+      };
 
-    const data = {
-      firstName,
-      lastName,
-      email,
-      number,
-      subject,
-      message,
-    };
-
-    fetch("https://mail.ledgermail.io/api/admin/website-contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Success:", data);
-        alert("Form submitted successfully!");
-        form.reset();
+      fetch("https://b2b-dev-admin-on-prem.ledgermail.io/api/v1/pre-register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
       })
-      .catch((error) => {
-        console.error("Error:", error);
-        alert("An error occurred. Please try again later.");
-      });
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Success:", data);
+          alert("Form submitted successfully!");
+          form.reset();
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          alert("An error occurred. Please try again later.");
+        });
+    } catch (error) {
+      console.error("Error:", error);
+      alert("An error occurred. Please try again later.");
+    }
   });
 });
---
+++ subscribescript.js
@@ -0,0 +1,34 @@
+document
+  .getElementById("subscribeForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const email = document.getElementById("emailid").value;
+
+    const data = { email: email };
+
+    fetch(
+      "https://b2b-dev-admin-on-prem.ledgermail.io//api/v1/pre-register/subscribe",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok " + response.statusText);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Success:", data);
+        // Optionally, you can display a success message to the user
+      })
+      .catch((error) => {
+        console.error("There was a problem with your fetch operation:", error);
+        // Optionally, you can display an error message to the user
+      });
+  });

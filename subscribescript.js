770ec7eed7429c0c70c2b8de538c6d5c0915fa7d
bharath-pingala <bharath@pingalasoftware.com>
2024-11-11 14:52:01 +0530

    updated UI and Content

--- subscribescript.js
+++ subscribescript.js
@@ -1,40 +1,82 @@
-document.getElementById("nav-about-us").addEventListener("click" , function () {
-  document.getElementById("about-section").scrollIntoView({ behavior: "smooth" });
-})
+document.getElementById('nav-about-us').addEventListener('click', function () {
+    document
+        .getElementById('about-section')
+        .scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('nav-security').addEventListener('click', function () {
+    document
+        .getElementById('platform-section')
+        .scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('footer-home').addEventListener('click', function () {
+    document
+        .getElementById('hero-section')
+        .scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('footer-card').addEventListener('click', function () {
+    document
+        .getElementById('card-section')
+        .scrollIntoView({ behavior: 'smooth' });
+});
 
 document
-  .getElementById("subscribeForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+    .getElementById('footer-about-us')
+    .addEventListener('click', function () {
+        document
+            .getElementById('about-section')
+            .scrollIntoView({ behavior: 'smooth' });
+    });
 
-    const email = document.getElementById("emailid").value;
+document
+    .getElementById('footer-security')
+    .addEventListener('click', function () {
+        document
+            .getElementById('platform-section')
+            .scrollIntoView({ behavior: 'smooth' });
+    });
 
-    const data = { email: email };
+document
+    .getElementById('subscribeForm')
+    .addEventListener('submit', function (event) {
+        event.preventDefault();
 
-    fetch(
-      "https://b2b-dev-admin-on-prem.ledgermail.io/api/v1/pre-register/subscribe",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      }
-    )
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok " + response.statusText);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        console.log("Success:", data);
-        alert(data.message);
+        const email = document.getElementById('emailid').value;
+        console.log('emailid: ', email);
 
-        // Optionally, you can display a success message to the user
-      })
-      .catch((error) => {
-        console.error("There was a problem with your fetch operation:", error);
-        // Optionally, you can display an error message to the user
-      });
-  });
+        const data = { email: email };
+
+        fetch(
+            'https://b2b-dev-admin-on-prem.ledgermail.io/api/v1/pre-register/subscribe',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            }
+        )
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(
+                        'Network response was not ok ' + response.statusText
+                    );
+                }
+                return response.json();
+            })
+            .then((data) => {
+                console.log('Success:', data);
+                alert(data.message);
+
+                // Optionally, you can display a success message to the user
+            })
+            .catch((error) => {
+                console.error(
+                    'There was a problem with your fetch operation:',
+                    error
+                );
+                // Optionally, you can display an error message to the user
+            });
+    });
